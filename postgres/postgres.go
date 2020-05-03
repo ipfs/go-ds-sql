3070a7b2e61c7b53cc51eda7a58bf1c9f1445493
Alan Shaw <alan.shaw@protocol.ai>
2020-05-03 08:52:34 +0100

    refactor: cache queries

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -21,52 +21,75 @@ type Options struct {
 
 // Queries are the postgres queries for a given table.
 type Queries struct {
-	TableName string
+	deleteQuery  string
+	existsQuery  string
+	getQuery     string
+	putQuery     string
+	queryQuery   string
+	prefixQuery  string
+	limitQuery   string
+	offsetQuery  string
+	getSizeQuery string
+}
+
+// NewQueries creates a new PostgreSQL set of queries for the passed table
+func NewQueries(tbl string) Queries {
+	return Queries{
+		deleteQuery:  fmt.Sprintf("DELETE FROM %s WHERE key = $1", tbl),
+		existsQuery:  fmt.Sprintf("SELECT exists(SELECT 1 FROM %s WHERE key=$1)", tbl),
+		getQuery:     fmt.Sprintf("SELECT data FROM %s WHERE key = $1", tbl),
+		putQuery:     fmt.Sprintf("INSERT INTO %s (key, data) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET data = $2", tbl),
+		queryQuery:   fmt.Sprintf("SELECT key, data FROM %s", tbl),
+		prefixQuery:  ` WHERE key LIKE '%s%%' ORDER BY key`,
+		limitQuery:   ` LIMIT %d`,
+		offsetQuery:  ` OFFSET %d`,
+		getSizeQuery: fmt.Sprintf("SELECT octet_length(data) FROM %s WHERE key = $1", tbl),
+	}
 }
 
 // Delete returns the postgres query for deleting a row.
 func (q Queries) Delete() string {
-	return fmt.Sprintf("DELETE FROM %s WHERE key = $1", q.TableName)
+	return q.deleteQuery
 }
 
 // Exists returns the postgres query for determining if a row exists.
 func (q Queries) Exists() string {
-	return fmt.Sprintf("SELECT exists(SELECT 1 FROM %s WHERE key=$1)", q.TableName)
+	return q.existsQuery
 }
 
 // Get returns the postgres query for getting a row.
 func (q Queries) Get() string {
-	return fmt.Sprintf("SELECT data FROM %s WHERE key = $1", q.TableName)
+	return q.getQuery
 }
 
 // Put returns the postgres query for putting a row.
 func (q Queries) Put() string {
-	return fmt.Sprintf("INSERT INTO %s (key, data) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET data = $2", q.TableName)
+	return q.putQuery
 }
 
 // Query returns the postgres query for getting multiple rows.
 func (q Queries) Query() string {
-	return fmt.Sprintf("SELECT key, data FROM %s", q.TableName)
+	return q.queryQuery
 }
 
 // Prefix returns the postgres query fragment for getting a rows with a key prefix.
-func (Queries) Prefix() string {
-	return ` WHERE key LIKE '%s%%' ORDER BY key`
+func (q Queries) Prefix() string {
+	return q.prefixQuery
 }
 
 // Limit returns the postgres query fragment for limiting results.
-func (Queries) Limit() string {
-	return ` LIMIT %d`
+func (q Queries) Limit() string {
+	return q.limitQuery
 }
 
 // Offset returns the postgres query fragment for returning rows from a given offset.
-func (Queries) Offset() string {
-	return ` OFFSET %d`
+func (q Queries) Offset() string {
+	return q.offsetQuery
 }
 
 // GetSize returns the postgres query for determining the size of a value.
 func (q Queries) GetSize() string {
-	return fmt.Sprintf("SELECT octet_length(data) FROM %s WHERE key = $1", q.TableName)
+	return q.getSizeQuery
 }
 
 // Create returns a datastore connected to postgres
@@ -79,7 +102,7 @@ func (opts *Options) Create() (*sqlds.Datastore, error) {
 		return nil, err
 	}
 
-	return sqlds.NewDatastore(db, Queries{TableName: opts.TableName}), nil
+	return sqlds.NewDatastore(db, NewQueries(opts.TableName)), nil
 }
 
 func (opts *Options) setDefaults() {
